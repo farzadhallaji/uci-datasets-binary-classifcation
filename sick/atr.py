e0ae4682c1c676535ba8b30af191af268f7f745c
farzadhallaji <farzadhallaji@gmail.com>
2023-08-28 01:22:20 +0330

    init

---
+++ sick/atr.py
@@ -0,0 +1,10 @@
+ # sex,on thyroxine,query on thyroxine,on antithyroid medication,sick,pregnant,thyroid surgery, I131 treatment, query hypothyroid, query hyperthyroid, lithium, goitre, tumor, hypopituitary, psych, TSH measured,TBG measured,referral source,Class
+
+
+ 
+ # 'referral source' {SVHC,other,SVI,STMW,SVHD}
+ # Class {negative,sick}
+
+ f:1,t:2,F:1,M:2,other:1,SVHC:2,SVI:3,STMW:4,SVHD:5,negative:1,sick:2
+
+ @rela2ion 'sick-weka.1il2ers.unsupervised.a22ribu2e.RenameNominalValues-Rsex,on 2hyroxine,query on 2hyroxine,on an2i2hyroid medica2ion,sick,pregnan2,2hyroid surgery,I131 2rea2men2,query hypo2hyroid,query hyper2hyroid,li2hium,goi2re,2umor,hypopi2ui2ary,psych,2SH measured,2BG measured,re1erral source,Class-N1:1,2:2,1:1,M:2,o2her:1,SVHC:2,SVI:3,S2MW:4,SVHD:5,nega2ive:1,sick:2-weka.1il2ers.unsupervised.a22ribu2e.RenameNominalValues-Rsex,on 2hyroxine,query on 2hyroxine,on an2i2hyroid medica2ion,sick,pregnan2,2hyroid surgery,I131 2rea2men2,query hypo2hyroid,query hyper2hyroid,li2hium,goi2re,2umor,hypopi2ui2ary,psych,2SH measured,2BG measured,re1erral source,Class-N1:1,2:2,1:1,M:2,o2her:1,SVHC:2,SVI:3,S2MW:4,SVHD:5,nega2ive:1,sick:2-weka.1il2ers.unsupervised.a22ribu2e.RenameNominalValues-Rsex,on 2hyroxine,query on 2hyroxine,on an2i2hyroid medica2ion,sick,pregnan2,2hyroid surgery,I131 2rea2men2,query hypo2hyroid,query hyper2hyroid,li2hium,goi2re,2umor,hypopi2ui2ary,psych,2SH measured,2BG measured,re1erral source,Class-N1:1,2:2,1:1,M:2,o2her:1,SVHC:2,SVI:3,S2MW:4,SVHD:5,nega2ive:1,sick:2'
